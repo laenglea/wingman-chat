6ab2207e2886ef0e9cb97723a77f2bb16be7fbd9
Adrian Liechti <adrian@monobox.ch>
2024-12-15 02:42:24 +0100

    add logo

--- src/components/ChatWindow.tsx
+++ src/components/ChatWindow.tsx
@@ -8,9 +8,13 @@ type ChatWindowProps = {
 export function ChatWindow({ messages }: ChatWindowProps) {
   return (
     <>
-      {messages.map((message, idx) => (
-        <ChatMessage key={idx} message={message} />
-      ))}
+      {messages.length === 0 ? (
+        <div className="flex flex-col items-center justify-center h-full text-center text-[#e5e5e5]">
+          <img src="/logo.png" className="w-48 h-48 mb-4" />
+        </div>
+      ) : (
+        messages.map((message, idx) => <ChatMessage key={idx} message={message} />)
+      )}
     </>
   );
 }
