0d3e1b01113a04a72647aa27d831371d3b80fd86
Adrian Liechti <adrian@monobox.ch>
2024-12-15 02:57:32 +0100

    move model selector to the left

--- src/components/ChatModel.tsx
+++ src/components/ChatModel.tsx
@@ -28,7 +28,7 @@ export function ChatModel({ models, selectedModel, onSelectModel }: ChatModelPro
       </button>
       
       {isOpen && (
-        <div className="absolute right-0 mt-2 w-40 bg-[#1c1c1e] border border-[#3a3a3c] rounded shadow-lg z-10">
+        <div className="absolute left-0 mt-2 w-40 bg-[#1c1c1e] border border-[#3a3a3c] rounded shadow-lg z-10">
           <ul className="py-1">
             {models.map((model) => (
               <li
